cff0ee22c77bd0be3149f445e38d0938afb73846
jACksEncheng <50646754+jACksEncheng@users.noreply.github.com>
2023-12-04 21:22:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from googleapiclient.discovery import build
 
 # 這裡填入你的API金鑰和Discord Webhook URL
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
-DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_TEST')
+DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 
 # YouTube API客戶端
 youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
@@ -38,7 +38,7 @@ def post_to_discord(channel_name, video_title, video_url):
     requests.post(DISCORD_WEBHOOK_URL, data=data)
 
 # 這裡填入要監控的YouTube頻道ID和關鍵字
-CHANNEL_IDS = ['UCxH2mFGJOqJ15UyCiZ7rN9w', 'UCpI7QnTiStXbCB3_Qnx96Tg']
+CHANNEL_IDS = ['UCxH2mFGJOqJ15UyCiZ7rN9w', 'UCpI7QnTiStXbCB3_Qnx96Tg','UCvN59KwVSCv0KaAUuAYyUew']
 KEYWORDS = ['戰隊戰']
 
 for channel_id in CHANNEL_IDS:
