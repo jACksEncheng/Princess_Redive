b74ef3217312d2af7c0fa6820ca7a90073bdf9dc
jACksEncheng <50646754+jACksEncheng@users.noreply.github.com>
2023-12-13 11:19:09 +0800

    mainv2.py を更新

---
+++ mainv2.py
@@ -0,0 +1,46 @@
+import os
+import datetime
+import requests
+from googleapiclient.discovery import build
+
+# 這裡填入你的API金鑰和Discord Webhook URL
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_TEST')
+
+# YouTube API客戶端
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+
+def check_videos(channel_id, keywords):
+    # 獲取頻道的最新影片
+    request = youtube.search().list(
+        part="snippet",
+        channelId=channel_id,
+        order="date",
+        type="video",
+        publishedAfter=(datetime.datetime.utcnow() - datetime.timedelta(days=7)).isoformat() + 'Z'
+    )
+    response = request.execute()
+
+    for item in response.get('items', []):
+        video_title = item['snippet']['title']
+        video_id = item['id']['videoId']
+        video_url = f"https://www.youtube.com/watch?v={video_id}"
+        video_description = item['snippet']['description']  # 添加視頻描述
+
+        # 檢查視頻標題是否包含任何關鍵字
+        if any(keyword.lower() in video_title.lower() for keyword in keywords):
+            post_to_discord(item['snippet']['channelTitle'], video_title, video_url, video_description)
+
+def post_to_discord(channel_name, video_title, video_url, video_description):
+    # 將信息發送到Discord
+    data = {
+        "content": f"新影片發布：{video_title}\n頻道：{channel_name}\n網址：{video_url}\n描述：{video_description}"
+    }
+    requests.post(DISCORD_WEBHOOK_URL, data=data)
+
+# 這裡填入要監控的YouTube頻道ID和關鍵字
+CHANNEL_IDS = ['UCxH2mFGJOqJ15UyCiZ7rN9w', 'UCpI7QnTiStXbCB3_Qnx96Tg','UCvN59KwVSCv0KaAUuAYyUew']
+KEYWORDS = ['戰隊戰']
+
+for channel_id in CHANNEL_IDS:
+    check_videos(channel_id, KEYWORDS)
