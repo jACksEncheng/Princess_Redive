235898c52be3b1d1b666fa33f00db299a1c9f524
jACksEncheng <50646754+jACksEncheng@users.noreply.github.com>
2024-01-04 13:36:02 +0800

    Update bilibili.py

--- bilibili.py
+++ bilibili.py
@@ -32,10 +32,8 @@ def cardToObj(input):
 async def send_to_discord(cardObj):
     if cardObj:  # 只有當 cardObj 不是空字典時才發送
         async with aiohttp.ClientSession() as session:
-            webhook_message = {
-                "content": json.dumps(cardObj, ensure_ascii=False)
-            }
-            await session.post(webhook_url, json=webhook_message)
+            # 直接發送字典對象，而不是序列化後的字符串
+            await session.post(webhook_url, json=cardObj)
 
 async def main():
     offset = 0
